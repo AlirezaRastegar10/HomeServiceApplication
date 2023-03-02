de6e5b33d74c925036ba42eaa458552f9299550f
phylix <alirezarastegar117390@gmail.com>
2023-03-03 01:12:34 +0330

    42 --> Creating UserNotFoundException class for handling not found user.

---
+++ final_project_phase4/src/main/java/com/example/exceptions/user/UserNotFoundExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.exceptions.user;
+
+
+import com.example.dto.exception.ResponseException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.sql.Timestamp;
+
+@ControllerAdvice
+public class UserNotFoundExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ResponseException> handleUserNotFoundException(UserNotFoundException exception, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        ResponseException responseException = new ResponseException(
+                new Timestamp(System.currentTimeMillis()),
+                httpStatus.value(),
+                httpStatus.name(),
+                exception.getMessage(),
+                request.getRequestURI());
+        return new ResponseEntity<>(responseException, httpStatus);
+    }
+}
