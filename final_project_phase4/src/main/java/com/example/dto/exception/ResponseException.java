ad0b72d5480eef4a847de9e80e424fa2525961ea
phylix <alirezarastegar117390@gmail.com>
2023-03-03 01:19:58 +0330

    57 --> Creating ResponseException class for return custom error message.

---
+++ final_project_phase4/src/main/java/com/example/dto/exception/ResponseException.java
@@ -0,0 +1,21 @@
+package com.example.dto.exception;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ResponseException {
+
+    Timestamp timestamp;
+    Integer status;
+    String error;
+    String message;
+    String path;
+}
