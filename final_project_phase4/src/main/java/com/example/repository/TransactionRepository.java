f1b0c17ceeee0558ca83bec5a5f5ee37675352ff
phylix <alirezarastegar117390@gmail.com>
2023-03-03 00:45:56 +0330

    22 --> Creating the TransactionRepository interface to communicate with the transaction table

---
+++ final_project_phase4/src/main/java/com/example/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.example.repository;
+
+
+import com.example.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long>, JpaSpecificationExecutor<Transaction> {
+}
