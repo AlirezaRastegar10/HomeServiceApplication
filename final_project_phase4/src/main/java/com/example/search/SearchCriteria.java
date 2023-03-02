d9e6d7c4b478663a28fcb226f0c1c97635acae69
phylix <alirezarastegar117390@gmail.com>
2023-03-03 01:09:44 +0330

    38 --> Creating search criteria class for advanced search

---
+++ final_project_phase4/src/main/java/com/example/search/SearchCriteria.java
@@ -0,0 +1,25 @@
+package com.example.search;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SearchCriteria {
+    String filterKey;
+    Object value;
+    String operation;
+    String dataOption;
+
+    public SearchCriteria(String filterKey, String operation, Object value) {
+        super();
+        this.filterKey = filterKey;
+        this.operation = operation;
+        this.value = value;
+    }
+}
---
+++ final_project_phase4/src/main/java/com/example/search/SearchDto.java
@@ -0,0 +1,17 @@
+package com.example.search;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SearchDto {
+    List<SearchCriteria> searchCriteriaList;
+    String dataOption;
+}
