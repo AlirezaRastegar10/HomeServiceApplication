464313fb36c372be99c9e54ff0e9ea3babf79dc4
phylix <alirezarastegar117390@gmail.com>
2023-03-03 01:21:05 +0330

    60 --> Creating FinalProjectPhase3Application class for running spring boot application.

---
+++ final_project_phase4/src/main/java/com/example/FinalProjectPhase4Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectPhase4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectPhase4Application.class, args);
+	}
+
+}
