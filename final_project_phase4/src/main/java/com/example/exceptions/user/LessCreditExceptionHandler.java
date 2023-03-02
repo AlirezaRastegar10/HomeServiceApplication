494294075cd619bea192444454b78a16cb4e1891
phylix <alirezarastegar117390@gmail.com>
2023-03-03 01:18:13 +0330

    54 --> Creating LessCreditException class for handling not enough inventory for payment order price

---
+++ final_project_phase4/src/main/java/com/example/exceptions/user/LessCreditExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.exceptions.user;
+
+
+import com.example.dto.exception.ResponseException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.sql.Timestamp;
+
+@ControllerAdvice
+public class LessCreditExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(LessCreditException.class)
+    public ResponseEntity<ResponseException> handleLessCreditException(LessCreditException exception, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+        ResponseException responseException = new ResponseException(
+                new Timestamp(System.currentTimeMillis()),
+                httpStatus.value(),
+                httpStatus.name(),
+                exception.getMessage(),
+                request.getRequestURI());
+        return new ResponseEntity<>(responseException, httpStatus);
+    }
+}
