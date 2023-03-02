3715603102a1589a0381cb3b0dbcfce144148f41
phylix <alirezarastegar117390@gmail.com>
2023-03-03 01:15:37 +0330

    48 --> Creating SubServiceNotFoundException class for handling sub service not found exception.

---
+++ final_project_phase4/src/main/java/com/example/exceptions/subservice/SubServiceNotFoundExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.exceptions.subservice;
+
+
+import com.example.dto.exception.ResponseException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.sql.Timestamp;
+
+@ControllerAdvice
+public class SubServiceNotFoundExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(SubServiceNotFoundException.class)
+    public ResponseEntity<ResponseException> handleSubServiceNotFoundException(SubServiceNotFoundException exception, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        ResponseException responseException = new ResponseException(
+                new Timestamp(System.currentTimeMillis()),
+                httpStatus.value(),
+                httpStatus.name(),
+                exception.getMessage(),
+                request.getRequestURI());
+        return new ResponseEntity<>(responseException, httpStatus);
+    }
+}
